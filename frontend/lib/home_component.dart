d8b03792180481aa70a6ca071177845c097fcaea
Dillon Nys <dillon.andre.nys@gmail.com>
2021-10-31 19:39:06 -0700

    Remove unused components

--- frontend/lib/home_component.dart
+++ frontend/lib/home_component.dart
@@ -4,7 +4,6 @@ import 'dart:convert';
 import 'dart:html';
 
 import 'package:angular/angular.dart';
-import 'package:amplify_explorer/controls/controls_component.dart';
 import 'package:amplify_explorer/editor/editor_component.dart';
 import 'package:amplify_explorer/service/transform_service.dart';
 import 'package:amplify_explorer/model/transform_request.g.dart';
@@ -27,7 +26,6 @@ import 'model/config.dart';
     FoCheckComponent,
     FoRadioComponent,
     FoRadioGroupComponent,
-    ControlsComponent,
     FoDropdownSelectComponent,
     FoLoadIndicatorComponent,
   ],
